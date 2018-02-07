72d06f963d619c9442876fcd5ceed91e2517fbc9
Rowan Meara <rowanmeara@gmail.com>
2018-02-07 15:01:45 -0800

    Install ffmpeg during setup.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt-get update -qq && \
 sudo apt-get install -y \
   pkg-config gfortran libhdf5-dev libfreetype6-dev liblapack-dev \
-  python python-setuptools python-dev python-pip python-tk
+  python python-setuptools python-dev python-pip python-tk ffmpeg
 
 sudo -H pip install --upgrade pip
 pip install --user numpy scipy matplotlib notebook pandas sympy nose scikit-learn h5py
